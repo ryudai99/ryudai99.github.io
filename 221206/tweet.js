2e49f051cd6027c9fda5a9b27395abc50de58e0f
ryudai99 <m0a21248b9@edu.teu.ac.jp>
2022-12-03 01:21:20 +0900

    自己紹介ページ

--- 221206/tweet.js
+++ 221206/tweet.js
@@ -18,11 +18,10 @@ let tweets = [
   { name: "三郎BOT", message: "19時でござる", tweetedAt: "2019-11-01 19:00:00", avatar: "https://morimorihoge.github.io/teu2019f/images/omocha_robot.png"  }
 ];
 
-for(let i=0;i<tweets.length;i++){
-  html += `<div style ="padding:1px;margin:0;border:1px solid #333333;"> <img src="${tweets[i].avatar}" alt="" width="80" height="100"> <b>${tweets[i].name}</b> ${tweets[i].message} <i>${tweets[i].tweetedAt}</i><br></div>`;
-}
-answer.innerHTML = html;
-
+for(let i = 0;i < tweets.length;i++) {
+    html += `<div style ="padding:1px;margin:0;border:1px solid #333333;"> <img src="${tweets[i].avatar}" alt="" width="80" height="100"> <b>${tweets[i].name}</b> ${tweets[i].message} <i>${tweets[i].tweetedAt}</i><br></div>`;
+  }
+  answer.innerHTML = html;
 
 let saburo2 =tweets.filter(tweet =>{
   return tweet.name === "三郎BOT";
@@ -39,18 +38,21 @@ let taro2 =tweets.filter(tweet =>{
 
 
 let saburo3 = function (){
+  saburo ="";
   for(let i=0;i<saburo2.length;i++){
     saburo += `<div style ="padding:1px;margin:0;border:1px solid #333333;"> <img src="${saburo2[i].avatar}" alt="" width="80" height="100"> <b>${saburo2[i].name}</b> ${saburo2[i].message} <i>${saburo2[i].tweetedAt}</i><br></div>`;
   }
   answer.innerHTML = saburo;
 };
 let jiro3 = function (){
+  jiro ="";
   for(let i=0;i<jiro2.length;i++){
     jiro += `<div style ="padding:1px;margin:0;border:1px solid #333333;"> <img src="${jiro2[i].avatar}" alt="" width="80" height="100"> <b>${jiro2[i].name}</b> ${jiro2[i].message} <i>${jiro2[i].tweetedAt}</i><br></div>`;
   }
   answer.innerHTML = jiro;
 };
 let taro3 = function (){
+  taro ="";
   for(let i=0;i<taro2.length;i++){
     taro += `<div style ="padding:1px;margin:0;border:1px solid #333333;"> <img src="${taro2[i].avatar}" alt="" width="80" height="100"> <b>${taro2[i].name}</b> ${taro2[i].message} <i>${taro2[i].tweetedAt}</i><br></div>`;
   }
